40f041e4016ce312639f1b5d0e63c80c9221676d
Lenmng Yeung <lenming.yeung@gmail.com>
2017-11-13 23:00:02 +1100

    Updated mirrors config.

--- project.clj
+++ project.clj
@@ -13,7 +13,11 @@
                  [yogthos/config "0.9"]
                  [com.taoensso/carmine "2.16.0"]]
   :plugins [[lein-ring "0.9.7"]]
-  :main env-ver-rest.core
+;            [brightnorth/uberjar-deploy "1.0.1"]]
+;  :aliases {"jar"    "uberjar"
+;                      "deploy" "uberjar-deploy"}
+;  :main ^:skip-aot env-ver-rest.core
+;  :main env-ver-rest.main
   :ring {:handler env-ver-rest.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
@@ -21,5 +25,11 @@
          :plugins [[com.jakemccrary/lein-test-refresh "0.21.1"]]
          :resource-paths ["config/dev"]}
    :test {:resource-paths ["config/test"]}
+   :cd {:resource-paths ["config/cd"]}
    :prod {:resource-paths ["config/prod"]}
-   })
+   :uberjar {:aot :all}
+   }
+
+   :target-path "target/%s/"
+           
+  )
